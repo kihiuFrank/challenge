193e8ec952b95667d1fe373af868a6327e32cc30
kihiuFrank <franklinekihiu@gmail.com>
2019-07-11 09:59:58 +0300

    first commit

---
+++ app/src/main/java/www/frank/alc4phase1/MainActivity.java
@@ -0,0 +1,36 @@
+package www.frank.alc4phase1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnAbout, btnProfile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAbout = findViewById(R.id.btnAbout);
+        btnProfile = findViewById(R.id.btnProfile);
+
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(this, About.class);
+//                startActivity(intent);
+            }
+        });
+
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, myProfile.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
